2f360179760ffe08ff8845b069460d1cb8ccfbe1
Jose Szychowski <joseszychowski@gmail.com>
2024-02-21 10:23:56 -0300

    feat: add router and apply error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Express } from 'express';
+import { shortenUrlRouter } from './routes/shortenUrl.route';
+import { errorHandler } from './middlewares/errorHandler.middleware';
 
 // App creation
 const app: Express = express();
@@ -8,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
+app.use('/api/v1/shortenUrl', shortenUrlRouter);
 
 // Error handling
+app.use(errorHandler);
 
 export { app };
