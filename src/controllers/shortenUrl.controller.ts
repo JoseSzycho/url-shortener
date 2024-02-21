a2c6b6134521a8affb7eb383b0e7f9d0160fdae1
Jose Szychowski <joseszychowski@gmail.com>
2024-02-21 12:04:29 -0300

    chore: change interface for dto

--- src/controllers/shortenUrl.controller.ts
+++ src/controllers/shortenUrl.controller.ts
@@ -1,8 +1,7 @@
 import { shortenUrlService } from '../services/shortenUrl.service';
 import ShortenUrlService from '../services/shortenUrl.service';
 import { Request, Response, NextFunction } from 'express';
-
-import { Iurl } from '../interfaces/url.interface';
+import { UrlDto } from '../dtos/urlDto.dto';
 
 class ShortenUrlController {
     private shortenUrlService: ShortenUrlService;
@@ -11,9 +10,9 @@ class ShortenUrlController {
     }
 
     create = async (req: Request, res: Response, next: NextFunction) => {
-        const url: Iurl = req.body;
+        const urlDto = new UrlDto(req.body);
         try {
-            const shortenUrl = await this.shortenUrlService.create(url);
+            const shortenUrl = await this.shortenUrlService.create(urlDto);
             res.status(200).json(shortenUrl);
         } catch (error) {
             next(error);
