76e3e3c41e1926d5aa43b417beba1d57ded74a9b
Jose Szychowski <joseszychowski@gmail.com>
2024-02-21 16:42:39 -0300

    feat: create interface

---
+++ src/dtos/getStats.dto.ts
@@ -0,0 +1,17 @@
+export class GetStatsDto {
+    key: string;
+    from: string;
+    skip: number | undefined;
+    take: number | undefined;
+    constructor(
+        key: string,
+        from: string,
+        skip: number | undefined,
+        take: number | undefined
+    ) {
+        this.key = key;
+        this.from = from;
+        this.skip = skip;
+        this.take = take;
+    }
+}
