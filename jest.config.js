08bebc005cd7b2493caf4e92cb54cc29a2d79cfb
Jose Szychowski <joseszychowski@gmail.com>
2024-02-22 18:43:05 -0300

    feat: configure jest tests

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageThreshold: {
+      global: {
+        branches: 65,
+        functions: 65,
+        lines: 65,
+        statements: 65,
+      },
+    },
+  };
